68ecfea3b5d5eb471dda22fd1f6b6cea90f6cb6b
JC <jcsegurat@gmail.com>
2025-06-06 14:28:41 -0600

    debug

--- src/components/CardQR.jsx
+++ src/components/CardQR.jsx
@@ -3,6 +3,7 @@ import * as UAParser from 'ua-parser-js';
 
 export default function CardQR({ title, description }) {
     const [movilOTablet, setMovilOTablet] = useState(false);
+    const [userAgent, setUserAgent] = useState('Sin datos ');
 
     useEffect(() => {
         const parser = new UAParser.UAParser();
@@ -15,11 +16,13 @@ export default function CardQR({ title, description }) {
         ) {
             setMovilOTablet(true);
         }
+        setUserAgent(navigator.userAgent);
     }, []);
 
     return (
         <div className="p-4 bg-white rounded-xl shadow-lg hover:shadow-2xl transform hover:-translate-y-1 transition-all duration-300 border-2 border-[#EC651B] flex flex-col items-center justify-center w-full group">
             <p>{movilOTablet ? "Móvil o Tableta" : "PC"}</p>
+            <p>userAgent: {userAgent}</p>
             <div className="flex-1 text-center sm:text-left">
                 <h2 className="text-xl font-semibold text-gray-800 mb-1">{title}</h2>
                 <p className="text-sm text-gray-800">{description}</p>
